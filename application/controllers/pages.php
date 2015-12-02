73e68ccb9585bf6a81e4ef9fb004ca4b3cee6568
Matt Eldridge <matt.eldridge@us.ibm.com>
2015-12-02 14:47:23 -0500

    Add conditional logic to home page.

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -1,5 +1,6 @@
 <?php
 class pages extends CI_Controller {
+
     public function view($page = 'home')
     {
         if (! file_exists('/app/codeigniter/application/views/pages/' . $page . '.php'))
@@ -11,8 +12,16 @@ class pages extends CI_Controller {
         $data['title'] = ucfirst($page);  // Capitalize the first letter
 
         $this->load->view('templates/header', $data);
-        $this->load->view('pages/' . $page, $data);
+
+        $twilio = $this->config->item('twilio');
+
+        if(empty($twilio['sid']) || empty($twilio['secret']))
+            $this->load->view('pages/credentials', $data );
+        else
+            $this->load->view('pages/' . $page, $data);
+
         $this->load->view('templates/footer', $data);
     }
+
 }
 ?>
