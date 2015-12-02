e05b1b2cd21037081af1f55db5a28e47c80787aa
Matt Eldridge <matt.eldridge@us.ibm.com>
2015-12-02 14:47:41 -0500

    Update about page.

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,2 +1,2 @@
-<p>This is the CodeIgniter Tutorial and it's running on CloudFoundry!.</p>
+<p>This is a CodeIgniter Tutorial and it's running on IBM BlueMix!.</p>
 <img src="/images/php-logo.gif" alt="PHP Logo" />
