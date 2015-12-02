c9ea7dd67e0f5af26472952f1f535ea80d48fd5a
Matt Eldridge <matt.eldridge@us.ibm.com>
2015-12-02 14:47:59 -0500

    Add telephone numbers form to home page.

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,5 +1,14 @@
-<h2>Home Page</h2>
-<p>Welcome!</p>
-<p><a href="/about">About</a></p>
-<p><a href="/news">View News</a></p>
-<p><a href="/news/create">Create News</a></p>
+<h2>Enter the Numbers to Call</h2>
+<form method="get" id="theForm" action="/api/bridge">
+    <div id="numbersContainer">
+        <div><input name="numbers[]" class="input" type="text" /><br/></div>
+    </div>
+    <br/><br/><input type="submit" /> &nbsp; <i class="fa fa-plus"></i>
+</form>
+<script>
+    $(document).ready(function(){
+        $('.fa-plus').on('click', function(){
+            $('input:text:first').parent().clone().appendTo('#numbersContainer').find("input[type='text']").val("");
+        });
+    });
+</script>
