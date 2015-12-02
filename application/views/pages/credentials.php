7a7fb16f930e054b834e4fa8dcb194ac64ba9fb7
Matt Eldridge <matt.eldridge@us.ibm.com>
2015-12-02 14:50:06 -0500

    Add page requesting that twilio credentials be added.

---
+++ application/views/pages/credentials.php
@@ -0,0 +1,5 @@
+<h2>Yikes!</h2>
+
+<p>You haven't added the keys to your Twilio account yet.</p>
+<p>This app won't work with out them.</p>
+<p>Add them to the bottom of the <code>application/config/config.php</code> file and restage the app.</p>
