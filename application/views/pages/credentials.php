687af3451c2ee9fdd1c67406e77bdfb9a784df81
Matt Eldridge <matt.eldridge@us.ibm.com>
2015-12-03 13:52:44 -0500

    Update credentials form.

--- application/views/pages/credentials.php
+++ application/views/pages/credentials.php
@@ -1,5 +1,25 @@
 <h2>Yikes!</h2>
 
 <p>You haven't added the keys to your Twilio account yet.</p>
-<p>This app won't work with out them.</p>
-<p>Add them to the bottom of the <code>application/config/config.php</code> file and restage the app.</p>
+<p>This app won't work without them.</p>
+<br/><br/>
+<p>Enter them here and submit this form.</p>
+<form method="post" id="theForm" action="/config/twilio">
+    <div id="numbersContainer">
+        <div>
+            <div class="col-md-6">Twilio SID</div>
+            <div class="col-md-6"><input name="sid" class="input" type="text" /></div>
+        </div>
+        <div>
+            <div class="col-md-6">Twilio Secret</div>
+            <div class="col-md-6"><input name="secret" class="input" type="text" /></div>
+        </div>
+        <div>
+            <div class="col-md-6">Twilio Validated Phone Number</div>
+            <div class="col-md-6"><input name="from_num" class="input" type="text" /></div>
+        </div>
+    </div>
+    <br/><br/><input type="submit" />
+</form>
+<br/><br/>
+<p>For a more permanent fix add them to the bottom of the <code>application/config/config.php</code> file and restage the app.</p>
