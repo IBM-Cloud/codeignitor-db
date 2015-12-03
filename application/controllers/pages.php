7efd3df2353db8c9b5ab5bdcced732a898b60841
Matt Eldridge <matt.eldridge@us.ibm.com>
2015-12-03 13:51:53 -0500

    Refactor and improve readbility of pages controller.

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -1,6 +1,11 @@
 <?php
 class pages extends CI_Controller {
 
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('session');
+    }
+
     public function view($page = 'home')
     {
         if (! file_exists('/app/codeigniter/application/views/pages/' . $page . '.php'))
@@ -13,14 +18,21 @@ class pages extends CI_Controller {
 
         $this->load->view('templates/header', $data);
 
-        $twilio = $this->config->item('twilio');
 
-        if(empty($twilio['sid']) || empty($twilio['secret']))
+        $twilio = $this->load_twilio();
+        if(empty($twilio['sid']) || empty($twilio['secret']) || empty($twilio['from_num']))
             $this->load->view('pages/credentials', $data );
         else
             $this->load->view('pages/' . $page, $data);
 
         $this->load->view('templates/footer', $data);
+    }
+
+    private function load_twilio() {
+        extract($this->config->item('twilio'));
+        if((empty($sid) || empty($secret)) && $this->session->userdata('twilio'))
+            extract($this->session->userdata('twilio'));
+        return array('sid' => $sid, 'secret' => $secret, 'from_num' => $from_num);
     }
 
 }
